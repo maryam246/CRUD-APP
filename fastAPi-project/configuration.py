12c01c774053adc34295a8541f091c80f160a9c0
Maryam khanam <143519643+maryam246@users.noreply.github.com>
2024-10-19 21:00:07 +0500

    Update configuration.py

--- fastAPi-project/configuration.py
+++ fastAPi-project/configuration.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://khanammaryam036:<YourPassword>@crud-app.pwgz0.mongodb.net/?retryWrites=true&w=majority&appName=CRUD-app"
+uri = "<Here, enter your mongodb url>"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
